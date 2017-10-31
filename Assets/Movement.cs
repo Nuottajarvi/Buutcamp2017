ba9f6646e332d2e60e987f16ad12df86b4ef6721
Nuottajarvi <pee-94@hotmail.com>
2017-10-31 13:54:22 +0200

    Buutcamp2017

---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddForce(transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            rb.AddForce(-transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            rb.AddTorque(-transform.up * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            rb.AddTorque(transform.up * speed);
+        }
+    }
+}
