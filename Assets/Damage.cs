b88c0eef4aa704baa99e4efcf7b00f41b122b0ae
Nuottajarvi <pee-94@hotmail.com>
2017-11-22 20:11:22 +0200

    damage and health

---
+++ Assets/Damage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Damage : NetworkBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        other.gameObject.GetComponent<Health>().TakeDamage();
+    }
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : MonoBehaviour {
+
+    int amount;
+
+	public void TakeDamage() {          
+        amount -= 1;
+    }
+}
