86c2e87334765e32561776e534ea16fb6e073b6b
Nuottajarvi <peetu.nuottajarvi@gmail.com>
2017-11-23 13:41:30 +0200

    texture loader

---
+++ Assets/GetTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetTexture : MonoBehaviour {
+
+    public GameObject rocket;
+    
+    // Use this for initialization
+    public IEnumerator Load (string url) {
+        WWW www = new WWW(url);
+
+        yield return www;
+
+        Renderer renderer = rocket.GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+	}
+
+    public void LoadTexture()
+    {
+        StartCoroutine(Load(GetComponent<InputField>().text));
+    }
+}
